712918ea97abc516d1d7d3c6c4a4ae59ea736069
Andrew <ATHur1104@gmail.com>
2023-06-27 22:51:15 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const inquirer = require("inquirer");
+const db = require('./db');
+
+// view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
